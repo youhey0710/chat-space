c152b6e079607775f4720783b6389ac4d54ebb0d
youhey0710 <youhei10140718@gmail.com>
2020-02-16 02:56:50 +0900

    変更

--- db/migrate/20200126054336_create_groups.rb
+++ db/migrate/20200126054336_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
-      t.index :name, null:unique: true
+      t.index :name, null: false, unique: true
       t.timestamps
     end
   end
