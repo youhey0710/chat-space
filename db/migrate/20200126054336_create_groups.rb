03669968c1eb6350c9dcb839b679feb63ce71c7f
youhey0710 <youhei10140718@gmail.com>
2020-02-16 15:04:02 +0900

    変更しました

--- db/migrate/20200126054336_create_groups.rb
+++ db/migrate/20200126054336_create_groups.rb
@@ -2,8 +2,9 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
-      t.index :name, null: unique: true
+      t.index :name, unique: true
       t.timestamps
     end
   end
 end
+bundle exec cap production deploy
